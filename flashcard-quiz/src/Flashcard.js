916a2f4cb5445a87dbe4dd12b31354b740067ce1
irishandyesiam <nathan.irish@gmail.com>
2023-03-29 14:55:25 -0500

    Added className static class 'card' with JS to set flip class.

--- flashcard-quiz/src/Flashcard.js
+++ flashcard-quiz/src/Flashcard.js
@@ -4,7 +4,10 @@ export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false)
 
     return (
-        <div onClick={() => setFlip(!flip)}>
+        <div 
+            className={`card ${flip ? 'flip' : ''}`}
+            onClick={() => setFlip(!flip)}
+        >
             {flip ? flashcard.answer : flashcard.question}
         </div>
     )
