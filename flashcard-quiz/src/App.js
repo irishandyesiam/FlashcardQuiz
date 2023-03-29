de61cf3e538bcf71b222ee4c86e1b894f8abb86a
irishandyesiam <nathan.irish@gmail.com>
2023-03-29 15:57:48 -0500

    HTTP call to API url set with axios + object values set for display + decode HTML to string.

--- flashcard-quiz/src/App.js
+++ flashcard-quiz/src/App.js
@@ -1,9 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState (SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios 
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)
+            ), 
+            answer
+          ]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: questionItem.correct_answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
   return (
     <FlashcardList flashcards={flashcards} />
   );
